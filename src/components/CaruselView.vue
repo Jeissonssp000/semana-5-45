29d1a9cec7be0ffb285969507fa9c232cdb9f0e5
Jeisson Salamanca <jeissonssp@gmail.com>
2020-12-23 18:53:27 -0500

    Subiendo imagen carousel

---
+++ src/components/CaruselView.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+  <b-carousel
+    id="carousel-fade"
+    style="text-shadow: 0px 0px 2px #000"
+    fade
+    indicators
+    img-width="1024"
+    img-height="480"
+  >
+    <b-carousel-slide
+      caption="First slide"
+      img-src="wallpaper.jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption="Second Slide"
+      img-src="https://picsum.photos/1024/480/?image=12"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption="Third Slide"
+      img-src="https://picsum.photos/1024/480/?image=22"
+    ></b-carousel-slide>
+  </b-carousel>
+</div>
+</template>
+
+<script>
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+export default {
+    name:"CaruselView",
+    data: () => ({
+      reveal: false,
+    }),
+}
+
+const options = {
+  transformAssetUrls: {
+    video: ['src', 'poster'],
+    source: 'src',
+    img: 'src',
+    image: 'xlink:href'
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/PageContent.vue
+++ src/components/PageContent.vue
@@ -1,42 +1,54 @@
 <template>
   <div class="container">
-    <v-container>
-      <v-row>
-        <v-col cols="13" sm="2">
-          <v-sheet rounded="lg" min-height="268">
-            <!--  -->
-            <img
-              class="imagen-three"
-              src="https://i.pinimg.com/474x/ab/26/67/ab26674c342e4e05d488ae20e24065c1.jpg"
-              alt="Avatar"
-            />
-          </v-sheet>
-        </v-col>
+    
+    <div>
+      <v-parallax
+        dark
+        src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+      >
+        <v-row align="center" justify="center">
+          <v-col class="text-center" cols="12">
+            <v-container>
+              <v-row>
+                <v-col cols="13" sm="2">
+                  <v-sheet rounded="lg" min-height="268">
+                    <!--  -->
+                    <img
+                      class="imagen-three"
+                      src="https://i.pinimg.com/474x/ab/26/67/ab26674c342e4e05d488ae20e24065c1.jpg"
+                      alt="Avatar"
+                    />
+                  </v-sheet>
+                </v-col>
 
-        <v-col cols="12" sm="8">
-          <v-sheet min-height="70vh" rounded="lg">
-            <!--  -->
-            <img
-              class="imagen-three-center"
-              src="https://www.sojodesign.es/gifs/animaciones/gif-4.gif"
-              alt="Megafono, megapromociones"
-              srcset=""
-            />
-          </v-sheet>
-        </v-col>
+                <v-col cols="12" sm="8">
+                  <v-sheet min-height="70vh" rounded="lg">
+                    <!--  -->
+                    <img
+                      class="imagen-three-center"
+                      src="https://www.sojodesign.es/gifs/animaciones/gif-4.gif"
+                      alt="Megafono, megapromociones"
+                      srcset=""
+                    />
+                  </v-sheet>
+                </v-col>
 
-        <v-col cols="12" sm="2">
-          <v-sheet rounded="lg" min-height="268">
-            <!--  -->
-            <img
-              class="imagen-three"
-              src="https://i.pinimg.com/474x/ab/26/67/ab26674c342e4e05d488ae20e24065c1.jpg"
-              alt="Avatar"
-            />
-          </v-sheet>
-        </v-col>
-      </v-row>
-    </v-container>
+                <v-col cols="12" sm="2">
+                  <v-sheet rounded="lg" min-height="268">
+                    <!--  -->
+                    <img
+                      class="imagen-three"
+                      src="https://i.pinimg.com/474x/ab/26/67/ab26674c342e4e05d488ae20e24065c1.jpg"
+                      alt="Avatar"
+                    />
+                  </v-sheet>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-col>
+        </v-row>
+      </v-parallax>
+    </div>
     <hr />
     <img
       src="https://3.bp.blogspot.com/-J8lZmpbKGr8/UfPavPl2OVI/AAAAAAAAtls/SXj4RSFWdbc/s1600/ivyvine.gif"
@@ -47,31 +59,28 @@
     <h3>Nuestros productos, pensados para tí</h3>
     <div class="container pb-5 pt-4">
       <div class="row">
-        <card-expand />
-        <card-expand />
-        <card-expand />
-        <card-expand />
-        <card-expand />
-        <card-expand />
-        <card-expand />
-        <card-expand />
-        <card-expand />
-        <card-expand />
-        <card-expand />
-        <card-expand />
+        <card-expand v-for="p in prices" :key="p"/>
       </div>
     </div>
+    <carusel-view/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import CardExpand from "./CardExpand.vue";
+import CaruselView from './CaruselView.vue';
 
 export default {
   name: "PageContent",
+  data(){
+    return{
+      prices: [8400, 1500, 2550, 3200, 6200, 1700, 2800]
+    }
+  },
   components: {
     CardExpand,
+    CaruselView,
   },
 };
 </script>
